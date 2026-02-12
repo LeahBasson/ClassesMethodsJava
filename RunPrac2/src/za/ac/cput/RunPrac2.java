3a44adf827ed7ad382c406d3fc559ab61209b0c2
LeahBasson <leahbasson54@gmail.com>
2026-02-12 17:10:21 +0200

    :sparkle:

--- RunPrac2/src/za/ac/cput/RunPrac2.java
+++ RunPrac2/src/za/ac/cput/RunPrac2.java
@@ -5,8 +5,7 @@ import javax.swing.JOptionPane;
 public class RunPrac2 {
     
     public static void main(String[] args) {
-        
-
+       
         // Can only use a String for input then you will have to use casting.
         String radInput = JOptionPane.showInputDialog("Enter radius: ");
         double radius = Double.parseDouble(radInput);
