7cf53423023774e61e815da566ede2baa6134ae3
LeahBasson <leahbasson54@gmail.com>
2026-02-12 17:17:50 +0200

    :sparkle:

--- RunPrac2/src/za/ac/cput/RunPrac2.java
+++ RunPrac2/src/za/ac/cput/RunPrac2.java
@@ -5,17 +5,17 @@ import javax.swing.JOptionPane;
 public class RunPrac2 {
     
     public static void main(String[] args) {
-       
+  
         // Can only use a String for input then you will have to use casting.
-        String radInput = JOptionPane.showInputDialog("Enter radius: ");
-        double radius = Double.parseDouble(radInput);
+        String input = JOptionPane.showInputDialog("Enter radius: ");
+        double radius = Double.parseDouble(input);
         
         // construct circle object
         Circle circle = new Circle(radius);
         
-        double diameter = circle.calDiameter();
+        circle.calDiameter();
         
-        JOptionPane.showMessageDialog(null, "The diameter is: " + diameter);
+        JOptionPane.showMessageDialog(null, "The diameter is: " + circle.getRadius());
         
     }
     
